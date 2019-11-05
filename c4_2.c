f91f32af501add453c1f3b0dc584f76dcf53415d
dcharlesDZL <dcharlesDZL@users.noreply.github.com>
2019-11-06 00:31:38 +0800

    add

--- c4_2.c
+++ c4_2.c
@@ -1,8 +1,13 @@
 #include "c4_2.h"
 #include <heapapi.h>
 #include <stdlib.h>
-
-
+#define SWAP(a, b){heaptype c;c =a; a = b; b = c;}
+//定义游标temp
+struct Temp
+{
+    heaptype num;
+    int index;
+}temp;
 void InitHeap(struct Heap* heap, int maxsize)
 {
     heap->num = (heaptype*)calloc(maxsize, sizeof(heaptype));
@@ -24,15 +29,38 @@ void ClearHeap(struct Heap* heap)
 }
 void InsertHeap(struct Heap* heap, heaptype x)
 {
-    if(heap->heapsize != 0)
+    if(heap)
     {
         heap->num[heap->heapsize] = x;
-        if(heap->heapsize%2 == 1)
+        //
+        temp.index = heap->heapsize;
+        temp.num = x;
+        while(temp.index != 0)//如果游标不为根节点则继续循环
         {
-            //if(heap->num[heap->heapsize] > heap->num[(heap->heapsize-1)/2 ])
+            if(temp.index%2 == 1)
+            {
+                //大于父节点，交换
+                if(temp.num > heap->num[(heap->heapsize-1)/2])
+                {
+                    SWAP(heap->num[temp.index], heap->num[(temp.index-1)/2]);
+                    temp.index = (temp.index-1)/2;//对游标重新赋值
+                }
+            }
+            else if(temp.index%2 ==0)
+            {
+                //大于父节点，交换
+                if(temp.num > heap->num[(heap->heapsize-2)/2])
+                {
+                    SWAP(heap->num[temp.index], heap->num[(temp.index-2)/2]);
+                    temp.index = (temp.index-2)/2;
+                }
+            }
         }
+        heap->heapsize++;//堆大小加1
     }
+    else printf("Heap is not initialized!");
 }
+
 int main()
 {
     struct Heap* aHeap;
