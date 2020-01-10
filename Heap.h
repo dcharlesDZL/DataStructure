442a931cf73e1925c82ae9c83b06d6238f71d623
dcharlesDZL <liketwins.ok@163.com>
2020-01-10 11:40:10 +0800

    add

--- Heap.h
+++ Heap.h
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include "Status.h"
 typedef int heaptype;
 struct Heap
 {
-    heaptype* num;
+    heaptype *num;
     int heapsize;
     int Maxsize;
 };
 /*********************/
 //初始化堆
-void InitHeap(struct Heap* heap, int maxsize);
+void InitHeap(struct Heap *heap, int maxsize);
 //清除堆
-void ClearHeap(struct Heap* heap);
+void ClearHeap(struct Heap *heap);
 //检查堆是否为空
-Bool isEmptyHeap(struct Heap* heap);
+Bool isEmptyHeap(struct Heap *heap);
 //向堆插入元素
-void InsertHeap(struct Heap* heap, heaptype x);
-
+void InsertHeap(struct Heap *heap, heaptype x);
